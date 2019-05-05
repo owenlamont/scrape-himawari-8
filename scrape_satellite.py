cfd570b023270c080ab0c8498c8017167544874d
Owen Lamont <owenrlamont@gmail.com>
2019-05-05 16:18:35 +0800

    Added new logic to resume partially downloaded files

--- scrape_satellite.py
+++ scrape_satellite.py
@@ -3,7 +3,7 @@ import datetime
 import os
 import shutil
 from pathlib import Path
-from typing import List, Set
+from typing import List, Set, Optional, Dict
 
 import pandas as pd
 import requests
@@ -34,11 +34,20 @@ def main(output_folder: Path, max_retries: int, look_back_days: int):
                 save_file_path: Path = output_folder / file_name
                 while not succeeded and try_counter < max_retries:
                     try:
-                        response = requests.get(url, stream=True, timeout=0.5)
-                        if response.status_code == 200:
-                            with open(save_file_path, "wb") as out_file:
-                                shutil.copyfileobj(response.raw, out_file)
-                                succeeded = True
+                        resume_header: Optional[Dict[str, str]] = None
+                        if os.path.isfile(save_file_path):
+                            offset = os.path.getsize(save_file_path)
+                            resume_header = {"Range": f"bytes={offset}-"}
+
+                        response = requests.get(url=url, headers=resume_header, stream=True, timeout=0.5)
+                        if response.status_code in [requests.codes.ok, requests.codes.partial_content]:
+                            if resume_header is None:
+                                with open(save_file_path, "wb") as out_file:
+                                    shutil.copyfileobj(response.raw, out_file)
+                            else:
+                                with open(save_file_path, "ab") as out_file:
+                                    shutil.copyfileobj(response.raw, out_file)
+                            succeeded = True
                         else:
                             try_counter += 1
                         del response
